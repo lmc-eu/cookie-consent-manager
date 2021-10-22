9283ac8eedb6a620bf81df9ac49402c967523d4c
Tomáš Litera <tomas.litera@lmc.eu>
2021-10-22 10:28:40 +0200

    Chore: Introduce publish package pipeline #CCM-8
      * based on release publishing

---
+++ bin/ci/semver.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+function grep_word_from_git_commits_since_last_tag() {
+    local commits=$(git log --oneline $(git describe --tags --abbrev=0 @^)..@ --grep="$1" 2> /dev/null)
+
+    # Check exit code of last command, eg. the one that gets the commits
+    # and normalize exit status if there is some unpredictable error (git or grep fail)
+    if [ $? -ne 0 ]; then
+        echo 2
+    elif [ -z "$commits" ];
+    then
+        echo 0
+    else
+        echo 1
+    fi
+}
+
+function get_version() {
+    if [ `grep_word_from_git_commits_since_last_tag "BREAKING CHANGE"` -gt 0 ]
+    then
+        echo "major"
+    elif [ `grep_word_from_git_commits_since_last_tag "Feat"` -gt 0 ]
+    then
+        echo "minor"
+    else
+        echo "patch"
+    fi
+}
+
+get_version
