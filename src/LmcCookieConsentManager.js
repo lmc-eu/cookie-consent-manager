23314b5115407153039f225455b391c04e54fa14
Ondřej Machulda <ondrej.machulda@lmc.eu>
2021-10-25 11:56:10 +0200

    Feat: Add callbacks for all onAccept scenarios #CCM-24

--- src/LmcCookieConsentManager.js
+++ src/LmcCookieConsentManager.js
@@ -8,7 +8,12 @@ import { config as configSk } from './languages/sk';
 const defaultOptions = {
   currentLang: 'cs',
   themeCss: '',
+  onFirstAccept: (cookie) => {},
+  onFirstAcceptOnlyNecessary: (cookie) => {},
+  onFirstAcceptAll: (cookie) => {},
   onAccept: (cookie) => {},
+  onAcceptOnlyNecessary: (cookie) => {},
+  onAcceptAll: (cookie) => {},
   config: {},
 };
 
@@ -16,20 +21,37 @@ const defaultOptions = {
  * @param {Object} args - Options for cookie consent manager
  * @param {string} args.currentLang - Specify one of the languages you have defined
  * @param {string} args.themeCss - Specify file to the .css file
- * @param {function} args.onAccept - Callback to be executed after any consent is detected (either given now or already saved previously)
+ * @param {function} args.onFirstAccept - Callback to be executed right after any consent is just accepted
+ * @param {function} args.onFirstAcceptOnlyNecessary - Callback to be executed right after only necessary cookies are accepted
+ * @param {function} args.onFirstAcceptAll - Callback to be executed right after all cookies are accepted
+ * @param {function} args.onAccept - Callback to be executed when any consent is detected (either given right now or already saved previously)
+ * @param {function} args.onAcceptOnlyNecessary - Callback to be executed when consent with only necessary cookies is detected (either given right now or already saved previously)
+ * @param {function} args.onAcceptAll - Callback to be executed when consent with all cookies is detected (either given right now or already saved previously)
  * @param {Object} args.config - Override default config. See https://github.com/orestbida/cookieconsent/blob/master/Readme.md#all-available-options
  */
 const LmcCookieConsentManager = (args) => {
   const options = { ...defaultOptions, ...args };
-  const { currentLang, themeCss, onAccept, config } = options;
+  const {
+    currentLang,
+    themeCss,
+    onFirstAccept,
+    onFirstAcceptOnlyNecessary,
+    onFirstAcceptAll,
+    onAccept,
+    onAcceptOnlyNecessary,
+    onAcceptAll,
+    config,
+  } = options;
+  const cookieName = 'lmc_ccm';
 
   const cookieconsent = window.initCookieConsent();
+  const isFirstTimeAccept = !cookieconsent.validCookie(cookieName);
 
   cookieconsent.run({
     current_lang: currentLang,
     auto_language: true,
     theme_css: themeCss,
-    cookie_name: 'lmc_ccm',
+    cookie_name: cookieName,
     cookie_expiration: 365,
     use_rfc_cookie: true,
     autorun: true,
@@ -44,6 +66,10 @@ const LmcCookieConsentManager = (args) => {
       },
     },
     onAccept: (cookie) => {
+      const givenLevels = cookieconsent.get('level');
+      const acceptedOnlyNecessary =
+        givenLevels.length === 1 && givenLevels[0] === 'necessary';
+
       window.dataLayer = window.dataLayer || [];
       window.dataLayer.push({
         event: 'CookieConsent-update',
@@ -56,6 +82,15 @@ const LmcCookieConsentManager = (args) => {
       });
 
       onAccept(cookie);
+
+      if (isFirstTimeAccept) {
+        onFirstAccept(cookie);
+        acceptedOnlyNecessary
+          ? onFirstAcceptOnlyNecessary(cookie)
+          : onFirstAcceptAll(cookie);
+      }
+
+      acceptedOnlyNecessary ? onAcceptOnlyNecessary(cookie) : onAcceptAll(cookie);
     },
     languages: {
       cs: configCs,
