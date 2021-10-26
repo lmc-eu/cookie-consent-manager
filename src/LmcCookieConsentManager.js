1bb6bd9ed6bcaad1d1e1ef8abc649a306c3278b2
Ondřej Machulda <ondrej.machulda@lmc.eu>
2021-10-26 15:03:01 +0200

    Feat: Return CookieConsent instance from the init function

--- src/LmcCookieConsentManager.js
+++ src/LmcCookieConsentManager.js
@@ -28,6 +28,7 @@ const defaultOptions = {
  * @param {function} [args.onAcceptOnlyNecessary] - Callback to be executed when consent with only necessary cookies is detected (either given right now or already saved previously)
  * @param {function} [args.onAcceptAll] - Callback to be executed when consent with all cookies is detected (either given right now or already saved previously)
  * @param {Object} [args.config] - Override default config. See https://github.com/orestbida/cookieconsent/blob/master/Readme.md#all-available-options
+ * @returns {Object} Instance of the underlying CookieConsent component. For available API, see https://github.com/orestbida/cookieconsent#apis--configuration-parameters
  */
 const LmcCookieConsentManager = (args) => {
   const options = { ...defaultOptions, ...args };
@@ -103,6 +104,8 @@ const LmcCookieConsentManager = (args) => {
     // override default config if necessary
     ...config,
   });
+
+  return cookieConsent;
 };
 
 export default LmcCookieConsentManager;
