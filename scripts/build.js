f21767df79f262d188a713e8af732a1c0383f649
Tomáš Litera <tomas.litera@lmc.eu>
2021-11-11 10:05:45 +0100

    Feat: Introduce build for CommonJS and use more common extensions

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,6 @@
 const { build } = require('esbuild');
+const fs = require('fs');
+const replace = require('replace-in-file');
 
 // iife
 build({
@@ -16,7 +18,7 @@ build({
   entryPoints: ['src/LmcCookieConsentManager.js'],
   bundle: true,
   target: 'es2017',
-  outfile: 'dist/LmcCookieConsentManager.esm.js',
+  outfile: 'dist/LmcCookieConsentManager.mjs',
   // format: 'esm', // or use platform: neutral and mainFields: ['main']
   platform: 'neutral', // or use format: 'esm' without  platform and mainFields
   /**
@@ -28,3 +30,37 @@ build({
   console.error(error);
   process.exit(1);
 });
+
+// cjs
+build({
+  entryPoints: ['src/LmcCookieConsentManager.js'],
+  bundle: true,
+  target: 'es6',
+  outfile: 'dist/LmcCookieConsentManager.cjs',
+  platform: 'node',
+  format: 'cjs',
+  /**
+   * the "main" field was ignored (main fields must be configured manually when using the "neutral" platform)
+   * because vanilla-cookie-consent is set as `main` in package.json
+   */
+  mainFields: ['main'],
+})
+  .then(() => {
+    const options = {
+      files: './dist/LmcCookieConsentManager.cjs',
+      from: /0 && \(module.exports = {}\);/gm,
+      to: `module.exports = LmcCookieConsentManager_default;`,
+    };
+
+    replace(options)
+      .then((results) => {
+        console.log('Replacement results:', results);
+      })
+      .catch((error) => {
+        console.error('Error occurred:', error);
+      });
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
