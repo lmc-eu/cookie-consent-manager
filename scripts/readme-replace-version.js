852fe2fa2d797cfa23463c7b6fa3f39d1b2d3da9
Ondřej Machulda <ondrej.machulda@lmc.eu>
2021-11-13 12:23:02 +0100

    Docs: Do not use patch part in URL to allow using bugfix versions automatically #CCM-29

--- scripts/readme-replace-version.js
+++ scripts/readme-replace-version.js
@@ -1,9 +1,12 @@
 const replace = require('replace-in-file');
 const packageJson = require('../package.json');
+// TODO: match just major version after 1.0 release
+const newVersion = packageJson.version.match(/([0-9]\.[0-9])\.[0-9]/)[1];
+
 const options = {
   files: 'README.md',
-  from: /cookie-consent-manager@([0-9]\.[0-9]\.[0-9])/gm,
-  to: `cookie-consent-manager@${packageJson.version}`,
+  from: /cookie-consent-manager@([0-9]\.[0-9])/gm, // TODO: replace with ([0-9]) after 1.0 release
+  to: `cookie-consent-manager@${newVersion}`,
 };
 
 replace(options)
