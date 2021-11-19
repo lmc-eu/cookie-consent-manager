82f5943b9382b1c6a32d4957d2a4aa0d360ca563
Tomáš Litera <tomas.litera@lmc.eu>
2021-11-19 15:47:03 +0100

    Chore: Fix building when using crypto in nanoid
      * error: Could not resolve "crypto" (use "platform: 'node'" when
        building for node)

--- scripts/build.js
+++ scripts/build.js
@@ -19,13 +19,7 @@ build({
   bundle: true,
   target: 'es2017',
   outfile: 'dist/LmcCookieConsentManager.mjs',
-  // format: 'esm', // or use platform: neutral and mainFields: ['main']
-  platform: 'neutral', // or use format: 'esm' without  platform and mainFields
-  /**
-   * the "main" field was ignored (main fields must be configured manually when using the "neutral" platform)
-   * because vanilla-cookie-consent is set as `main` in package.json
-   */
-  mainFields: ['main'],
+  format: 'esm',
 }).catch((error) => {
   console.error(error);
   process.exit(1);
