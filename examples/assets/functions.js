2bd3398fe5dc2f5d2c53b3336ec933530a0f3ef3
Tomáš Litera <tomas.litera@lmc.eu>
2021-12-02 12:40:42 +0100

    Style: Use additional insights of eslint's base config as recommended

--- examples/assets/functions.js
+++ examples/assets/functions.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 /**
  * Global function to reset cookie for the examples only
+ *
+ * @returns {void}
  */
 function removeCookieAndReload() {
   document.cookie = `lmc_ccm=; Max-Age=0; path=/; domain=${window.location.hostname}`;
