09aa8e6c6abad8d68b7c14476ba8df689925d569
Tomáš Litera <tomas.litera@lmc.eu>
2021-12-14 14:52:04 +0100

    Chore: Annotate the CommonJS export names for ESM import in Node
      * we need to build commonjs module as `browser` to avoid nanoid using
        crypto module which is native node.js module
      * instead of that we want to use web crypto api
      * using `platform:browser` bundler finds right export from nanoid and
        use it
      * but for this platform bundler did not annotate the commonjs export
        so we must do it manually

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 const { build } = require('esbuild');
-const replace = require('replace-in-file');
+const fs = require('fs');
 
 // iife
 build({
@@ -31,30 +31,25 @@ build({
   bundle: true,
   target: 'es6',
   outfile: 'dist/LmcCookieConsentManager.cjs',
-  platform: 'node',
+  platform: 'browser',
   format: 'cjs',
   /**
    * the "main" field was ignored (main fields must be configured manually when using the "neutral" platform)
    * because vanilla-cookie-consent is set as `main` in package.json
    */
   mainFields: ['main'],
-})
-  .then(() => {
-    const options = {
-      files: './dist/LmcCookieConsentManager.cjs',
-      from: /0 && \(module.exports = {}\);/gm,
-      to: 'module.exports = LmcCookieConsentManager_default;',
-    };
-
-    replace(options)
-      .then((results) => {
-        console.log('Replacement results:', results);
-      })
-      .catch((error) => {
-        console.error('Error occurred:', error);
-      });
-  })
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+}).then(() => {
+  // Annotate the CommonJS export names for ESM import in node
+  fs.appendFile(
+    './dist/LmcCookieConsentManager.cjs',
+    `// Annotate the CommonJS export names for ESM import in node:
+module.exports = LmcCookieConsentManager_default;
+`,
+    (error) => {
+      if (error) {
+        console.log(error);
+        process.exit(1);
+      }
+    },
+  );
+});
