9d282d5401b00098b3df9035f1a8114406751a0f
Tomáš Litera <tomas.litera@lmc.eu>
2021-12-17 17:14:35 +0100

    Chore: Trigger deploy preview only when certain directories changed
      * using custom netlify build hook
      * @see https://docs.netlify.com/configure-builds/build-hooks/

---
+++ bin/ci/deploy-preview.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+branch = $1
+
+function has_code_changes() {
+  git diff --quiet HEAD main -- src examples
+
+  if [ $? -eq 0 ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function trigger_build() {
+  curl -X POST -d '{}' https://api.netlify.com/build_hooks/61bca39f3b89823193476ecd?trigger_branch=${branch}
+}
+
+has_code_changes && trigger_build
+
