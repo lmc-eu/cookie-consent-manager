ee81161c42ef1d78530019912fd87823cad88270
Tomáš Litera <tomas.litera@lmc.eu>
2021-12-17 17:56:20 +0100

    Chore(commitlint): Remove ignore sections cause it is not used
      * there are no pull request merge commits
      * we are not using dependabot anymore
      * we are not using ci-skip

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,15 +1,3 @@
 module.exports = {
   extends: ['@lmc-eu/commitlint-config'],
-  ignores: [
-    (commit) => commit.includes('[ci-skip]'),
-    (commit) => commit.includes('Pull request'),
-    /**
-     * Ignore Dependabot commits because they are violating sentence case rule
-     * and cannot be configured or forced to respect commitlint configuration.
-     *
-     * @see https://github.com/dependabot/dependabot-core/issues/1666
-     * @see https://github.com/dependabot/dependabot-core/issues/406
-     */
-    (commit) => commit.includes('Deps: Bump'),
-  ],
 };
