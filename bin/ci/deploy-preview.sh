0b81e42ab96777764ac3a6dbb6dc15d74eb56148
Tomáš Litera <tomas.litera@lmc.eu>
2021-12-17 17:30:00 +0100

    fixup! Chore: Trigger deploy preview only when certain directories changed

--- bin/ci/deploy-preview.sh
+++ bin/ci/deploy-preview.sh
@@ -5,15 +5,21 @@ function has_code_changes() {
   git diff --quiet HEAD origin/main -- src examples
 
   if [ $? -eq 0 ]; then
+    echo "No code changes detected."
+
     return 0
   else
+    echo "Code changes detected."
+
     return 1
   fi
 }
 
 function trigger_build() {
+  echo "Triggering build..."
+
   curl -X POST -d '{}' https://api.netlify.com/build_hooks/61bca39f3b89823193476ecd?trigger_branch=$1
 }
 
-has_code_changes && trigger_build
+has_code_changes || trigger_build
 
