894c85360f2611529a8eae67a01bcccae27c344c
Ondřej Machulda <ondrej.machulda@lmc.eu>
2022-02-11 12:29:33 +0100

    Docs: List allowed categories on callbacks example page

--- examples/assets/functions.js
+++ examples/assets/functions.js
@@ -16,3 +16,22 @@ function toggleLanguage() {
 
   window.ccm.updateLanguage(selectedLanguage);
 }
+
+/**
+ * @param {Object} cookieConsent
+ * @returns {void}
+ */
+function updateAllowedCategories(cookieConsent) {
+  for (const element of document.querySelectorAll('.list-categories .list-group-item-success')) {
+    element.classList.remove('list-group-item-success');
+  }
+
+  for (const category of ['necessary', 'ad', 'analytics', 'functionality', 'personalization']) {
+    const element = document.getElementById(`consent-${category}`);
+    if (cookieConsent.allowedCategory(category)) {
+      element.classList.add('list-group-item-success');
+    } else {
+      element.classList.remove('list-group-item-success');
+    }
+  }
+}
