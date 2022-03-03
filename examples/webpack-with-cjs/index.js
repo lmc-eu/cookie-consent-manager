52ae52b1dd9c58f29d3896fa0af123a1c88b2eec
Tomáš Litera <tomas.litera@lmc.eu>
2022-03-03 10:11:22 +0100

    Docs: Use consent configuration in examples

--- examples/webpack-with-cjs/index.js
+++ examples/webpack-with-cjs/index.js
@@ -1,6 +1,18 @@
 // eslint-disable-next-line import/no-unresolved
 const LmcCookieConsentManager = require('@lmc-eu/cookie-consent-manager');
+// eslint-disable-next-line import/no-unresolved
+const { DisplayMode } = require('@lmc-eu/cookie-consent-manager');
 
-window.addEventListener('load', () => {
-  LmcCookieConsentManager('example');
+const ccmArgs = {
+  displayMode: DisplayMode.SOFT,
+  config: {
+      cookie_name: 'lmc_ccm_example',
+      force_consent: true,
+      cookie_domain: 'example.com',
+      cookie_necessary_only_expiration: 30,
+  }
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  LmcCookieConsentManager('example', ccmArgs);
 });
