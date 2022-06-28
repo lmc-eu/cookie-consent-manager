5c517f2a9fb30db356d6b2b6bcaea90c6280aa39
Ondřej Machulda <ondrej.machulda@lmc.eu>
2022-06-28 12:53:29 +0200

    Test: Add SecondaryButtonMode to typescript example

--- examples/webpack-with-typescript/index.ts
+++ examples/webpack-with-typescript/index.ts
@@ -1,8 +1,9 @@
 // eslint-disable-next-line import/no-unresolved
-import LmcCookieConsentManager, { DisplayMode } from '@lmc-eu/cookie-consent-manager';
+import LmcCookieConsentManager, { DisplayMode, SecondaryButtonMode } from '@lmc-eu/cookie-consent-manager';
 
 const ccmArgs = {
   displayMode: DisplayMode.SOFT,
+  secondaryButtonMode: SecondaryButtonMode.SHOW_SETTINGS,
   config: {
       cookie_name: 'lmc_ccm_example',
       cookie_domain: 'example.com',
