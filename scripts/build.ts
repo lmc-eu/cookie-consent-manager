e758d8589193cb9b14b36c6ed7945b1b8cfdc469
Ondřej Machulda <ondrej.machulda@lmc.eu>
2024-10-08 14:39:11 +0200

    BREAKING CHANGE: Remove CommonJS suppport

--- scripts/build.ts
+++ scripts/build.ts
@@ -27,34 +27,4 @@ build({
   process.exit(1);
 });
 
-// cjs
-build({
-  entryPoints: ['src/LmcCookieConsentManager.ts'],
-  bundle: true,
-  target: 'es6',
-  outfile: 'dist/LmcCookieConsentManager.cjs',
-  platform: 'browser',
-  format: 'cjs',
-  /**
-   * the "main" field was ignored (main fields must be configured manually when using the "neutral" platform)
-   * because vanilla-cookie-consent is set as `main` in package.json
-   */
-  mainFields: ['main'],
-  tsconfig: 'tsconfig.build.json',
-}).then(() => {
-  // Annotate the CommonJS export names for ESM import in node
-  fs.appendFile(
-    './dist/LmcCookieConsentManager.cjs',
-    `// Annotate the CommonJS export names for ESM import in node:
-module.exports = LmcCookieConsentManager_default;
-`,
-    (error: unknown) => {
-      if (error) {
-        console.log(error);
-        process.exit(1);
-      }
-    },
-  );
-});
-
 export {};
