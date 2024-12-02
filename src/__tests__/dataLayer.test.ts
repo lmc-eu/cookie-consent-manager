72d8e73009f4161bac555a652b23b9e6ceb04da9
Ondřej Machulda <ondrej.machulda@lmc.eu>
2024-12-02 17:40:19 +0100

    Refactor: Update pushToDataLayer to new internal cookie value structure

--- src/__tests__/dataLayer.test.ts
+++ src/__tests__/dataLayer.test.ts
@@ -1,24 +1,32 @@
 import { pushToDataLayer } from '../dataLayer';
-import { CookieConsentCategoryValues, VanillaCookieConsent } from '../types';
-import { CookieConsentCategory } from '../constants';
+import { CookieValue } from 'vanilla-cookieconsent';
 
 describe('dataLayer', () => {
   describe('pushToDataLayer', () => {
-    it('should pushToDataLayer', () => {
-      expect(true).toBe(true);
-    });
-
     beforeEach(() => {
       // Reset the dataLayer before each test
       window.dataLayer = [];
     });
 
     it('should push the correct event to the dataLayer', () => {
-      const cookie: VanillaCookieConsent.Cookie<CookieConsentCategoryValues> = {
-        level: [CookieConsentCategory.NECESSARY, CookieConsentCategory.ANALYTICS],
-        data: { uid: '123', serviceName: 'foo-service' },
+      const cookie: CookieValue = {
+        categories: ['necessary', 'analytics'],
         revision: 1,
-        rfc_cookie: true,
+        data: {
+          serviceName: 'demo.example',
+          uid: 'foo-bar-baz',
+        },
+        consentTimestamp: '2024-12-12T12:12:12.333Z',
+        consentId: 'f6152595-45a0-4371-aa68-2fe97137111a',
+        services: {
+          necessary: [],
+          ad: [],
+          analytics: [],
+          functionality: [],
+          personalization: [],
+        },
+        lastConsentTimestamp: '2024-12-12T12:12:12.333Z',
+        expirationTime: 1764690333666,
       };
 
       pushToDataLayer(cookie);
@@ -37,11 +45,24 @@ describe('dataLayer', () => {
     });
 
     it('should handle empty levels correctly', () => {
-      const cookie: VanillaCookieConsent.Cookie<CookieConsentCategoryValues> = {
-        level: [],
-        data: { uid: '123', serviceName: 'foo-service' },
+      const cookie: CookieValue = {
+        categories: [],
         revision: 333,
-        rfc_cookie: true,
+        data: {
+          serviceName: 'demo.example',
+          uid: 'foo-bar-baz',
+        },
+        consentTimestamp: '2024-12-12T12:12:12.333Z',
+        consentId: 'f6152595-45a0-4371-aa68-2fe97137111a',
+        services: {
+          necessary: [],
+          ad: [],
+          analytics: [],
+          functionality: [],
+          personalization: [],
+        },
+        lastConsentTimestamp: '2024-12-12T12:12:12.333Z',
+        expirationTime: 1764690333666,
       };
 
       pushToDataLayer(cookie);
