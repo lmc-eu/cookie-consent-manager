99e6af43625b5f1d7c79e71730975ded031cde6d
Ondřej Machulda <ondrej.machulda@lmc.eu>
2024-12-10 16:57:56 +0100

    Docs: Update examples to new API

--- examples/assets/functions.js
+++ examples/assets/functions.js
@@ -14,21 +14,21 @@ function removeCookieAndReload() {
 function toggleLanguage() {
   const selectedLanguage = Array.from(document.getElementsByName('lang-select')).find(r => r.checked).value;
 
-  window.ccm.updateLanguage(selectedLanguage);
+  window.ccm.setLanguage(selectedLanguage);
 }
 
 /**
  * @param {object} cookieConsent
  * @returns {void}
  */
-function updateAllowedCategories(cookieConsent) {
+function updateAcceptedCategories(cookieConsent) {
   for (const element of document.querySelectorAll('.list-categories .list-group-item-success')) {
     element.classList.remove('list-group-item-success');
   }
 
   for (const category of ['necessary', 'ad', 'analytics', 'functionality', 'personalization']) {
     const element = document.getElementById(`consent-${category}`);
-    if (cookieConsent.allowedCategory(category)) {
+    if (cookieConsent.acceptedCategory(category)) {
       element.classList.add('list-group-item-success');
     } else {
       element.classList.remove('list-group-item-success');
