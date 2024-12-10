4c5ad2deb35a815a9518277cc6f986cbbac5fda8
Ondřej Machulda <ondrej.machulda@lmc.eu>
2024-12-10 13:53:32 +0100

    Docs: Update example builds to match vanilla-cookieconsent v3

--- examples/webpack-with-esm/index.js
+++ examples/webpack-with-esm/index.js
@@ -4,10 +4,11 @@ import { DisplayMode, LmcCookieConsentManager } from '@lmc-eu/cookie-consent-man
 const ccmArgs = {
   displayMode: DisplayMode.SOFT,
   config: {
-      cookie_name: 'lmc_ccm_example',
-      force_consent: true,
-      cookie_domain: 'example.com',
-      cookie_necessary_only_expiration: 30,
+    cookie: {
+      name:  'lmc_ccm_example',
+      domain: 'example.com',
+      expiresAfterDays: 365,
+    }
   }
 };
 
